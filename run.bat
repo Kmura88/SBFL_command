7ea558119a93afa76ecb61232053970e8da9383c
Kmura88 <u900065d@ecs.osaka-u.ac.jp>
2025-10-03 01:50:09 +0900

    .bat update

--- run.bat
+++ run.bat
@@ -1,5 +1,9 @@
+rem %1 実行するjavaのmain関数があるクラス
+
 @echo off
 cd /d %~dp0
+
+java -javaagent:lib/jacocoagent.jar=destfile=jacoco.exec -cp classes %1
 
 java -jar lib/jacococli.jar report jacoco.exec --classfiles classes --sourcefiles src --xml Report.xml
 
