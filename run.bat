e88f64034df2c57953bf16adf6b39d249b517746
Kmura88 <u900065d@ecs.osaka-u.ac.jp>
2025-10-03 02:08:29 +0900

    run.bat update

--- run.bat
+++ run.bat
@@ -1,9 +1,16 @@
-rem %1 実行するjavaのmain関数があるクラス
-
 @echo off
 cd /d %~dp0
 
-java -javaagent:lib/jacocoagent.jar=destfile=jacoco.exec -cp classes %1
+rem %1 実行するjavaのmain関数があるクラス
+
+set CLASS_NAME=%1
+
+if "%CLASS_NAME%"=="" (
+	echo [error] usage : run.bat class
+	exit /b
+)
+
+java -javaagent:lib/jacocoagent.jar=destfile=jacoco.exec -cp classes %CLASS_NAME% 
 
 java -jar lib/jacococli.jar report jacoco.exec --classfiles classes --sourcefiles src --xml Report.xml
 
