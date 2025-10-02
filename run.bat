8aa7f7dbbe8283d73c74e0d42a21cfb9c2a93b0a
Kmura88 <u900065d@ecs.osaka-u.ac.jp>
2025-10-03 02:48:16 +0900

    exec ファイル生成場所の変更

--- run.bat
+++ run.bat
@@ -1,17 +1,31 @@
 @echo off
 cd /d %~dp0
 
-rem %1 実行するjavaのmain関数があるクラス
-
 set CLASS_NAME=%1
+set TMP_FOLDER=.SBFL_data
 
+rem 構文チェック
 if "%CLASS_NAME%"=="" (
 	echo [error] usage : run.bat class
 	exit /b
 )
 
-java -javaagent:lib/jacocoagent.jar=destfile=jacoco.exec -cp classes %CLASS_NAME% 
+rem 一時フォルダが既にあれば削除 
+if exist "%TMP_FOLDER%" (
+	rd /s /q %TMP_FOLDER%
+)
 
-java -jar lib/jacococli.jar report jacoco.exec --classfiles classes --sourcefiles src --xml Report.xml
+rem 一時フォルダの作成
+mkdir %TMP_FOLDER%
+attrib +h %TMP_FOLDER%
 
-python ./lib/command.py
+rem .execの生成
+java -javaagent:lib/jacocoagent.jar=destfile=%TMP_FOLDER%/jacoco.exec,append=false -cp classes %CLASS_NAME%
+
+rem .xmlの生成
+java -jar lib/jacococli.jar report %TMP_FOLDER%/jacoco.exec --classfiles classes --sourcefiles src --xml Report.xml
+
+rem .xmlデータの読み込み
+python ./lib/command.py 
+
+rd /s /q %TMP_FOLDER%
