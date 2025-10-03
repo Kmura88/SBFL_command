41dbc73b6aef3dcaa5301ad15b5fd6bb255321b7
Kmura88 <u900065d@ecs.osaka-u.ac.jp>
2025-10-04 02:54:52 +0900

    微更新

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # How to use
 
+classesフォルダにpackage通りの階層構造でクラスファイルを入れる。javaプログラム実行に必要。
+もしpackageがcom.exampleでクラスファイルがMainTestなら、
+
+```
+classes/
+└── com/
+    └── example/
+        └── MainTest.class
+```
+
+srcフォルダも同様にする。
+
+```
+src/
+└── com/
+    └── example/
+        └── MainTest.java
+```
+
+
 現状はクラスファイルに対するSBFL
 今後はソースコードが必要。GumTreeするのに。
 
--- run.bat
+++ run.bat
@@ -6,13 +6,25 @@ set TMP_FOLDER=.SBFL_data
 
 rem 構文チェック
 if "%CLASS_NAME%"=="" (
-	echo [error] usage : run.bat class
+	echo [Error] usage : run.bat class
 	exit /b
 )
 
-rem 一時フォルダが既にあれば削除 
+rem 一時フォルダが既にあれば一旦削除 
 if exist "%TMP_FOLDER%" (
 	rd /s /q %TMP_FOLDER%
+)
+
+rem classesフォルダが有るか
+if not exist "classes" (
+	echo [Error] classes Folder not found.
+	exit /b
+)
+
+rem srcフォルダが有るか
+if not exist "src" (
+	echo [Error] src Folder not found.
+	exit /b
 )
 
 rem 一時フォルダの作成
