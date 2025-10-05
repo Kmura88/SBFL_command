faaf6585a777f28369646e915d8fff263011d646
Kmura88 <u900065d@ecs.osaka-u.ac.jp>
2025-10-05 17:00:32 +0900

    LineExecutionCounterの実装

--- lib/LineExecutionCounter.py
+++ lib/LineExecutionCounter.py
@@ -1,13 +1,14 @@
 import XmlAnalyzer as XA #XA.XmlAnalyzer(".")
+from pathlib import Path
 import sys
 
 
 class LineExcutionCounter:
 	def __init__(self,file_path):
-		self.file_path = file_path  # xmlがあるファイルへのパス
-		self.excution_counter = {}  # Map< クラス名, 各クラスの行ごとの実行回数int[] >
-		self.total_counter = 0      # テストの実行回数
-		self.xml_path = XA.XmlAnalyzer(".")
+		self.file_path = Path(file_path)  # xmlがあるファイルへのパス
+		self.excution_counter = {}        # Map< クラス名, 各クラスの行ごとの実行回数int[] >
+		self.total_counter = 0            # テストの実行回数
+		self._calc_counter()
 
 	def get_excutionCounter(self):
 		return self.excution_counter
@@ -16,10 +17,22 @@ class LineExcutionCounter:
 		return self.total_counter
 	
 	def print_excution_counter(self):
-		for cls, arr in self.coverages.items():
+		for cls, arr in self.excution_counter.items():
 			print(f"Class: {cls}")
 			for i in range(1, len(arr)):  # 行番号は1スタート
 				print(f"  Line {i}: {arr[i]}")
+
+	def _calc_counter(self):
+		xml_paths = self.file_path.rglob('*') # .xmlファイルを全探索
+		for xml_path in xml_paths:
+			XmlAnalyzer = XA.XmlAnalyzer(xml_path)
+			coverages = XmlAnalyzer.get_coverages()
+			for cls, bools in coverages.items():
+				if cls not in self.excution_counter:
+					self.excution_counter[cls] = [0] * len(bools) # 初回実行時の初期化
+				for i, covered in enumerate(bools):
+					if covered:
+						self.excution_counter[cls][i] += 1  # 実行された回数を加算
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
